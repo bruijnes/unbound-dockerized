f419a10631a3c6e26b096a14bed85d1613f3a3e9
Vincent Bruijnes <v.bruijnes@skymedicalgroup.com>
2024-06-04 23:28:11 +0200

    change license, credits and some configuration stuff

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,4 @@
 #!/bin/sh
-set -e
-
-# Create necessary devices inside the chroot
-mkdir -p /app/dev
-mknod -m 666 /app/dev/null c 1 3
-mknod -m 666 /app/dev/random c 1 8
-mknod -m 666 /app/dev/urandom c 1 9
-mknod -m 666 /app/dev/zero c 1 5
-mknod -m 666 /app/dev/stdout c 1 3
-mknod -m 666 /app/dev/stderr c 1 3
-mknod -m 666 /app/dev/stdin c 1 3
-
-# Create necessary log file
-touch /app/dev/log
 
 # Ensure permissions are correct
 chown -R unbound:unbound /app/dev
