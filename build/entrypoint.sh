81cc004b77d91c168d16fa25c8b130e08d19e064
Vincent Bruijnes <v.bruijnes@skymedicalgroup.com>
2024-06-05 13:19:21 +0200

    latest changes

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
 
-# Create required username and group for chroot to work
-groupadd unbound
-useradd -r -g unbound unbound
-
 # Ensure permissions are dset on the workdir (/app)
 chown -R unbound:unbound /app
+
+# Write unbound config
+cat > /app/unbound.conf <<EOL
+server:
+    directory: "/app"
+    chroot: "/app"
+    pidfile: "/app/unbound.pid"
+    username: unbound
+    use-syslog: no
+    logfile: ""
+    verbosity: 2
+    log-queries: yes
+    interface: ${INTERFACE_V4}
+    interface: ${INTERFACE_V6}
+    access-control: ${ALLOW_V4} allow
+    #access-control: ${ALLOW_V6} allow
+EOL
 
 # Ensure RW permissions of config file
 chmod 0600 /app/unbound.conf
